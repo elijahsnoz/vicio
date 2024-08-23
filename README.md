d68e1042f2eaaeda4d95592e34ae21f4d032e359
elijahsnoz <elijahsnoz@gmail.com>
2024-08-23 12:12:19 +0100

    vicio

---
+++ README.md
@@ -0,0 +1,4 @@
+Addiction breaker - joint project
+Project by Elijah & Adeola
+Email : iadeola2055@gmail.com
+Email : elijahsnoz@gmail.com
---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Form submitted successfully!");
+});
+
